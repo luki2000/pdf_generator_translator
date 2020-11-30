9af4c2b5bf7ed69dc06423544a2c1c03981bbf1a
lukindo mbuli <lukindo.mbuli@richemont.com>
2020-11-30 17:20:29 +0100

    playing with angular 9 pdfmake and localize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import '@angular/localize/init';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gen-pdf';
+  pdfMake: any;
+  firstName = 'henry';
+  lastName = 'kissinger';
+  number = 746547;
+  // lazy load pdf lib which is over 3mb
+  loadPdfMaker() {
+    if (!this.pdfMake) {
+      // lazyLoading in component ts is not supported by default pre-angular 9
+      // its preferable to lazy load because library is +3.7MB
+      // const pdfMakeModule = await import('pdfmake/build/pdfmake');
+      // const pdfFontsModule = await import('pdfmake/build/vfs_fonts');
+      // this.pdfMake = pdfMakeModule.default;
+      // this.pdfMake.vfs = pdfFontsModule.default.pdfMake.vfs;
+    }
+  }
+
+   generatePdf() {
+    // this.loadPdfMaker();
+    // const def = { content: 'A sample PDF document generated using Angular and PDFMake' };
+    // this.pdfMake.createPdf(def).open();
+    // in ie11 you cannot open pdf to new tab without downloading
+    const documentDefinition = { content: $localize`:@@reference.number: ${this.number}` };
+    pdfMake.createPdf(documentDefinition).download();
+  }
 }
